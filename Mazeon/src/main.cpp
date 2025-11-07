33b4ebb1efddeb7c9ed1c9c680c47a3892895cd6
HHChandar20 <HHChandar20@codingburgas.bg>
2025-11-07 05:48:05 +0200

    Add raylib dependency and basic window setup

--- Mazeon/src/main.cpp
+++ Mazeon/src/main.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include "raylib.h"
 
 int main() 
 {
-    std::cout << "Mazeon" << std::endl;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+
+    InitWindow(screenWidth, screenHeight, "Mazeon");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) 
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        
+        DrawText("Mazeon - Maze Game", 20, 20, 20, WHITE);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
